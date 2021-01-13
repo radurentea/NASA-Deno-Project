18bac7ab089c6ad6e5557ef455e9008beefea819
Radu Rentea <radu.rentea@gmail.com>
2021-01-13 09:15:10 +0200

    Practice middlewares

--- mod.ts
+++ mod.ts
@@ -3,11 +3,24 @@ import { Application } from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 const app = new Application();
 const PORT = 8000;
 
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get('X-Response-Time');
+  console.log(`${ctx.request.method}, ${ctx.request.url}: ${time}`);
+})
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${delta}ms`);
+});
+
 app.use((ctx) => {
   ctx.response.body = 'Hello World';
 });
 
-if(import.meta.main) {
+if (import.meta.main) {
   await app.listen({
     port: PORT
   });
