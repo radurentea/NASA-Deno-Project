51cb45c477551940315454c5ec4d832dec3c42bd
Radu Rentea <radu.rentea@gmail.com>
2021-01-16 22:48:24 +0200

    Create launches.ts

---
+++ models/launches.ts
@@ -0,0 +1,52 @@
+import * as log from 'https://deno.land/std/log/mod.ts';
+import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
+
+interface Launch{
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: Array<string>;
+  launchDate: number;
+  upcoming: boolean;
+  success?: boolean;
+  target?: string;
+}
+
+const launches = new Map<number, Launch>();
+
+export async function downloadLaunchData() {
+  log.info('Downloading launch data...');
+  const response = await fetch('https://api.spacexdata.com/v3/launches', {
+    method: 'GET',
+  });
+
+  if (!response.ok) {
+    log.warning('Problem fetching launch data.');
+    throw new Error('Launch data donwload failed.');
+  }
+
+  const launchData = await response.json();
+  for (const launch of launchData) {
+    const payloads = launch['rocket']['second_stage']['payloads'];
+    const customers = _.flatMap(payloads, (payload : any) => {
+      return payload['customers'];
+    });
+
+    const flightData = {
+      flightNumber: launch['flight_number'],
+      mission: launch['mission_name'],
+      rocket: launch['rocket']['rocket_name'],
+      launchDate: launch['launch_date_unix'],
+      upcoming: launch['upcoming'],
+      success: launch['launch_success'],
+      customers,
+    };
+
+    launches.set(flightData.flightNumber, flightData);
+
+    log.info(JSON.stringify(flightData));
+  }
+}
+
+  await downloadLaunchData();
+  log.info(`Donwloaded data for ${ launches.size } SpaceX launches.`);
