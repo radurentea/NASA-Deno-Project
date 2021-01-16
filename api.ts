ba109bc8584dcb76ca8cdbf8d8ed8d8a9e69e66a
Radu Rentea <radu.rentea@gmail.com>
2021-01-16 13:39:29 +0200

    Add planets API model

--- api.ts
+++ api.ts
@@ -1,9 +1,15 @@
 import { Router } from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 
+import * as planets from './models/planets.ts';
+
 const router = new Router();
 
 router.get('/', (ctx) => {
   ctx.response.body = 'Hello world';
 });
+
+router.get('/planets', (ctx) => {
+  ctx.response.body = planets.getAllPlanets();
+})
 
 export default router;
